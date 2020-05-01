bd9c1bc9b9c57933b17c53d2220a4389d57aa928
Timothy Brackett <brackett.tc@gmail.com>
2020-05-01 03:41:15 +0100

    Run tests in detached tmux session

---
+++ run_in_tmux.sh
@@ -0,0 +1,30 @@
+#!/bin/sh
+
+# Copyright (c) 2020 Timothy Brackett
+# Licensed under the MIT license
+
+tmpscript=$(mktemp)
+tmpcode=$(mktemp)
+tmpoutput=$(mktemp)
+
+trap 'rm ${tmpscript} ${tmpcode}' EXIT
+
+# create a temporary script to pass the exit code back through a temp file
+cat <<EOF > ${tmpscript}
+#!/bin/sh
+
+$@ > ${tmpoutput} 2>&1
+echo \$? > ${tmpcode}
+EOF
+
+tmux new -d -s 'bg-session' "sh ${tmpscript}"
+
+# wait for tmux session to end
+while tmux has-session -t 'bg-session' 2> /dev/null; do
+    true
+done
+
+cat ${tmpoutput}
+
+# read exit code and return that ourselves
+exit $(cat ${tmpcode})
