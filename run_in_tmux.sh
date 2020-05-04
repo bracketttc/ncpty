b00be45909a61d21390f5c62599382f655ac1bc0
Timothy Brackett <brackett.tc@gmail.com>
2020-05-04 02:01:44 +0100

    Add double-quotes to prevent word-splitting

--- run_in_tmux.sh
+++ run_in_tmux.sh
@@ -7,24 +7,24 @@ tmpscript=$(mktemp)
 tmpcode=$(mktemp)
 tmpoutput=$(mktemp)
 
-trap 'rm ${tmpscript} ${tmpcode}' EXIT
+trap 'rm "${tmpscript}" "${tmpcode}"' EXIT
 
 # create a temporary script to pass the exit code back through a temp file
-cat <<EOF > ${tmpscript}
+cat <<EOF > "${tmpscript}"
 #!/bin/sh
 
-$@ > ${tmpoutput} 2>&1
-echo \$? > ${tmpcode}
+$@ > "${tmpoutput}" 2>&1
+echo \$? > "${tmpcode}"
 EOF
 
-tmux new -d -s 'bg-session' "sh ${tmpscript}"
+tmux new -d -s 'bg-session' "sh \"${tmpscript}\""
 
 # wait for tmux session to end
 while tmux has-session -t 'bg-session' 2> /dev/null; do
     true
 done
 
-cat ${tmpoutput}
+cat "${tmpoutput}"
 
 # read exit code and return that ourselves
-exit $(cat ${tmpcode})
+exit "$(cat "${tmpcode}")"
