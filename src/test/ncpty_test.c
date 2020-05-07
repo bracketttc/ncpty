1b88c887888bf493890b6df6b3710f655ffe54dd
Timothy Brackett <brackett.tc@gmail.com>
2020-05-07 06:07:31 +0100

    Start developing unit tests

---
+++ src/test/ncpty_test.c
@@ -0,0 +1,38 @@
+#include "greatest.h"
+#include "ncpty.h"
+
+
+TEST invalid_status_tests( void )
+{
+    ASSERT_FALSE( ncpty_status( NULL, NULL ) );
+
+    int code = 0;
+    ASSERT_FALSE( ncpty_status( NULL, &code ) );
+    ASSERT_EQ( -1, code );
+
+    PASS();
+}
+
+
+TEST invalid_free_test( void )
+{
+    ncpty_free( NULL );
+
+    struct ncpty_t* pty = NULL;
+    ncpty_free( &pty );
+
+    PASS();
+}
+
+
+GREATEST_MAIN_DEFS();
+
+int main( int argc, char** argv )
+{
+    GREATEST_MAIN_BEGIN();
+
+    RUN_TEST( invalid_status_tests );
+    RUN_TEST( invalid_free_test );
+
+    GREATEST_MAIN_END();
+}
